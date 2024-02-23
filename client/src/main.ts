11ca35b3be2614ed65443e8e0dbb3040c37b4fb0
ufuksaylan <ufuksaylannn@gmail.com>
2024-02-23 16:53:06 +0200

    add app instance to sentry config client

--- client/src/main.ts
+++ client/src/main.ts
@@ -10,6 +10,7 @@ const app = createApp(App)
 if (sentryClientDsn) {
   console.log('Sentry enabled')
   Sentry.init({
+    app,
     dsn: sentryClientDsn,
     integrations: [
       Sentry.browserTracingIntegration(),
@@ -21,6 +22,8 @@ if (sentryClientDsn) {
     // Performance Monitoring
     tracesSampleRate: 1.0, //  Capture 100% of the transactions
     // Session Replay
+    logErrors: true, // Log errors to console in addition to sending to Sentry
+    attachProps: true, // Attach Vue component props
     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
   })
